8f7420c023dfa6e037e522a67d3e17a2d829bb39
samkelsey <samjkelsey1@gmail.com>
2023-05-16 22:04:16 +0100

    test: testing identifier and keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -23,13 +23,27 @@ func Test_lexer(t *testing.T) {
 				{token.NUMBER, "678"},
 			},
 		},
+		{
+			name:  "lex identifier token",
+			input: "false1",
+			expected: []token.Token{
+				{token.IDENTIFIER, "false1"},
+			},
+		},
+		{
+			name:  "lex keyword with identifier",
+			input: "fun hello",
+			expected: []token.Token{
+				{token.FUNCTION, "fun"},
+				{token.IDENTIFIER, "hello"},
+			},
+		},
 	}
 
 	for _, tc := range testCases {
 		tc := tc
 
 		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
 
 			l := New(tc.input)
 			got, err := l.Lex()
